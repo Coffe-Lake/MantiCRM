d857218139f8ffee32f51a104e2a86f742788274
Coffe-Lake <felixkot00@gmail.com>
2022-01-14 13:02:56 +0300

    Update URls

--- orders/urls.py
+++ orders/urls.py
@@ -6,15 +6,12 @@ app_name = 'orders'
 
 urlpatterns = [
     path('', OrdersListView.as_view(), name='orders_list'),
-    # path('new_order/', NewOrderView.as_view(), name='new_order'),
     path('pre_orders/', PreOrdersListView.as_view(), name='pre_orders_list'),
     path('completed_orders/', CompletedOrdersListView.as_view(), name='completed_orders_list'),
     path('order_detail/<int:pk>/', OrderDetailView.as_view(), name='order_detail'),
     path('order_detail/<int:pk>/check', CheckDetailView.as_view(), name='check'),
+    # path('order_update/<int:pk>/update', OrderUpdateView.as_view(), name='update_order'),
 
     # ________ CRUD ________
     path('create_order', CreateOrder.as_view(), name='create_order'),
-
-    # ________ AJAX ________
-    # path('set_status', set_status, name='set_status'),
 ]
