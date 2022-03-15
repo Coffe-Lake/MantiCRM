66974713069e1e3337dc7ae779f5e18449106d4c
Coffe-Lake <felixkot00@gmail.com>
2022-03-15 23:17:37 +0300

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
